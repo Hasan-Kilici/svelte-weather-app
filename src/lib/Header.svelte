8fb67f9a18d21db0e53f820bfdf9a59cf832cd0f
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-11 17:22:33 +0300

    Create Header.svelte

---
+++ src/lib/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+    <a href="/">Svelte Weather App</a>
+
+</header>
+
+
+<style>
+    header {
+        overflow: hidden;
+        background-color: red;
+        font-family: Arial;
+        text-align: center;
+        padding: 1rem;
+        margin:0;
+    }
+   header  a {
+            color: white;
+            text-decoration: none;
+            cursor: pointer;
+            font-size: 2rem;
+        }
+</style>
