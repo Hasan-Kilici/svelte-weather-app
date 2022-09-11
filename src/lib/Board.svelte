bed5948bd311b638ea93c8741e45071a742f5959
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-11 17:22:21 +0300

    Create Board.svelte

---
+++ src/lib/Board.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { weatherList } from './store.js';
+    import WeatherCard from './WeatherCard.svelte';
+</script>
+
+
+{#each $weatherList as weather, index}
+    <WeatherCard {weather} {index} />
+{/each}
